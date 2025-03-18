60590d482f869687f479fcf7f04670ffaabe1c63
Mazzulli <mazzulli@live.com>
2025-03-18 20:19:22 -0300

    feat: updated next.config.ts file to allow remote images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { SignOut } from "@/components/sign-out";
 import { auth } from "@/lib/auth";
+import Image from "next/image";
 import { redirect } from "next/navigation";
+import userDefaultImage from "@/assets/user-circle.svg";
 
 const Page = async () => {
   const session = await auth();
@@ -8,9 +10,16 @@ const Page = async () => {
 
   return (
     <>
-      <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
+      <div className="bg-gray-100 rounded-lg p-4 text-center mb-6 justify-center flex flex-col items-center">
         <p className="text-gray-600">Logado como:</p>
         <p className="font-medium">{session.user?.email}</p>
+        <Image
+          src={session.user?.image || userDefaultImage}
+          alt={session.user?.email || "Usuário"}
+          width={100}
+          height={100}
+          className="rounded-full mt-8"
+        />
       </div>
 
       <SignOut />
